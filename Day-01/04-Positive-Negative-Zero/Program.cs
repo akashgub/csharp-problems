d53f08c4276e65d9d840713db79b245d918acebe
akash <akashgub21@gmail.com>
2026-09-13 20:20:11 +0600

    Add Day 1 Problem 4

---
+++ Day-01/04-Positive-Negative-Zero/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.WriteLine(number + " is a positive number.");
+}
+else if (number < 0)
+{
+    Console.WriteLine(number + " is a negative number.");
+}
+else
+{
+    Console.WriteLine("The number is zero.");
+}

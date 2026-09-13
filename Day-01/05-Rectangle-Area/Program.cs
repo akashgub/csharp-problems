f47ce76209b83c7adae15f60a9b76d0713c48a7f
akash <akashgub21@gmail.com>
2026-09-13 23:31:20 +0600

    Add Day 1 Problem 5

---
+++ Day-01/05-Rectangle-Area/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter length: ");
+double length = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter width: ");
+double width = Convert.ToDouble(Console.ReadLine());
+
+double area = length * width;
+Console.WriteLine("The area of the rectangle is: " + area);

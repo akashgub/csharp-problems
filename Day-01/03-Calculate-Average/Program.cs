445915d78009d7abfba555ec6686e3b73381f0c9
akash <akashgub21@gmail.com>
2026-09-13 19:15:10 +0600

    Add Day 1 Problem 3

---
+++ Day-01/03-Calculate-Average/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your first number: ");
+int a = Convert.ToInt32(Console.ReadLine()); //int a = 15
+
+Console.Write("Enter your second number: ");
+int b = Convert.ToInt32(Console.ReadLine()); // int b = 25
+
+Console.Write("Enter your third number: ");
+int c = Convert.ToInt32(Console.ReadLine()); // int c = 35
+
+double average = (a + b + c) / 3.0;
+
+Console.WriteLine("The average of " + a + ", " + b + ", and " + c + " is: " + average);

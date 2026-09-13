b399e189fed0cc3acfb2122efa5391ebc87f5efe
akash <akashgub21@gmail.com>
2026-09-14 00:11:19 +0600

    Add Day 2 Problem 5

---
+++ Day-02/05-Multiplication-Table/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine()); // int a = 15
+
+for (int i = 1; i <= 10; i++)
+{
+    int ans = number * i;
+    Console.WriteLine(number + " x " + i + " = " + ans);
+}

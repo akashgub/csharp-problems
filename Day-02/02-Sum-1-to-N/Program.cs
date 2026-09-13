a414c82301c5cc4420588ac7d5ff6db03c1b2e19
akash <akashgub21@gmail.com>
2026-09-13 23:54:02 +0600

    Add Day 2 Problem 2

---
+++ Day-02/02-Sum-1-to-N/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= N; i++)
+{
+    sum += i;
+}
+Console.WriteLine("The sum of numbers from 1 to " + N + " is: " + sum);

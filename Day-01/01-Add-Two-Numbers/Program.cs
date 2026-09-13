41522284fd61ce7d0544a7c245832bfc34fb73ed
akash <akashgub21@gmail.com>
2026-09-13 12:09:03 +0600

    Add Day 1 Problem 1

---
+++ Day-01/01-Add-Two-Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, Akash! Welcome to the C# programming world.");
+
+Console.Write("Enter your first number: ");
+int a = Convert.ToInt32(Console.ReadLine()); //int a = 15
+
+Console.Write("Enter your second number: ");
+int b = Convert.ToInt32(Console.ReadLine()); // int b = 25
+// int sum = a + b;
+Console.WriteLine("The sum of " + a + " and " + b + " is: " + (a + b));
+
+

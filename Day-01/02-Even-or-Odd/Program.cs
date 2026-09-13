408042cb902b653dc4ee7ef2c3a0867aec02d98f
akash <akashgub21@gmail.com>
2026-09-13 19:01:35 +0600

    Add Day 1 Problem 2

---
+++ Day-01/02-Even-or-Odd/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a number: ");
+int a = Convert.ToInt32(Console.ReadLine()); // int a = 15
+
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " is an even number.");
+}
+else
+{
+    Console.WriteLine(a + " is an odd number.");
+}

a78e48bff2fd568af8ff4d8a734971cfcc57b8b4
akash <akashgub21@gmail.com>
2026-09-14 17:39:56 +0600

    Add Day 03 Problem 05 - Find Largest Number

---
+++ Day-03/05-Find-Largest-Number/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a >= b && a >= c)
+{
+    Console.WriteLine("Largest number = " + a);
+}
+else if (b >= a && b >= c)
+{
+    Console.WriteLine("Largest number = " + b);
+}
+else
+{
+    Console.WriteLine("Largest number = " + c);
+}
+

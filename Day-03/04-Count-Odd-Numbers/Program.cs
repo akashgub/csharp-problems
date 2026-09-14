5e3f3fd57368ceea14959804f72cf56fb83fe0c7
akash <akashgub21@gmail.com>
2026-09-14 15:01:15 +0600

    Add Day 03 Problem 04 - Count Odd Numbers

---
+++ Day-03/04-Count-Odd-Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+ int count = 0;
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 != 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Count of odd numbers = " + count);
